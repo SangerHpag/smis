41ceae8b8cc924bf46438af27fda0b18f42665ad
Francesca Giordano <fgiordano.data@gmail.com>
2018-02-19 14:06:58 +0000

    Added example test with E.coli data

--- example/run_ecoli_test.sh
+++ example/run_ecoli_test.sh
@@ -2,10 +2,13 @@
 
 folder=`pwd`
 
-$folder/../setup.sh $folder/smis_test $folder/ecoli_data/draft_assembly.fasta $folder/ecoli_data/ont.fastq &> log.out
+$folder/../setup.sh $folder/smis_test $folder/ecoli_data/draft_assembly.fasta $folder/ecoli_data/ont.fastq &> exmaple.log
 
 cd $folder/smis_test
-./mysmissv.sh  &>> log.out
+./mysmissv.sh  &>> example.log
 
 
-echo Scaffolds are in  smis_test/spinner_scaffolds.fasta
+echo; echo Scaffolds are in  smis_test/spinner_scaffolds.fasta
+echo example log output: exmaple.log
+echo smis logs in smis_test/logs/
+echo
